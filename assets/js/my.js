3b2b0b957c291ebbcfc33f23deeba623ea71bf2d
ufo22940268 <ufo22940268@gmail.com>
2013-06-07 19:21:17 +0800

    asdf

--- assets/js/my.js
+++ assets/js/my.js
@@ -96,46 +96,6 @@ function showTinyLoading() {
 showLoading();
 showTinyLoading();
 
-$(document).ready(function() {
-    $('#reportrange').daterangepicker(
-	{
-	    ranges: {
-			'Today': [new Date(), new Date()],
-	'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-	'Last 7 Days': [moment().subtract('days', 6), new Date()],
-	'Last 30 Days': [moment().subtract('days', 29), new Date()],
-	'This Month': [moment().startOf('month'), moment().endOf('month')],
-	'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-		    },
-	opens: 'left',
-	format: 'MM/DD/YYYY',
-	separator: ' to ',
-	startDate: moment().subtract('days', 29),
-	endDate: new Date(),
-	minDate: '01/01/2012',
-	maxDate: '12/31/2013',
-	locale: {
-	    applyLabel: 'Submit',
-	fromLabel: 'From',
-	toLabel: 'To',
-	customRangeLabel: 'Custom Range',
-	daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr','Sa'],
-	monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-	firstDay: 1
-	},
-	showWeekNumbers: true,
-	buttonClasses: ['btn-danger'],
-	dateLimit: false
-	},
-    function(start, end) {
-	$('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-    }
-);
-//Set the initial state of the picker label
-$('#reportrange span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
-
-});
-
 function updateSelectionState() {
     if ($(".row-selector:checked").length == 0) {
         $("#select-batch-ops").css("visibility", "hidden");
@@ -212,3 +172,14 @@ function saveMutibleItemStates(op) {
         loadContent();
     })
 }
+
+$(document).ready(function() {
+    $('#daterange').daterangepicker(
+        {
+            minDate: '01/01/2010',
+    maxDate: '12/31/2015',
+    showDropdowns: true
+        }
+        );
+
+});
